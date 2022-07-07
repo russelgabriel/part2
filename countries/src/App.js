edb869dcf575ee0c6e708446537d07bcee789282
russelsy <russelgabrielsy@gmail.com>
2022-07-07 11:20:30 -0400

    finished 2.14

--- countries/src/App.js
+++ countries/src/App.js
@@ -7,13 +7,14 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [searchCountry, setSearchCountry] = useState("")
   const [chosenCountry, setChosenCountry] = useState({})
+  const [weatherInfo, setWeatherInfo] = useState({})
 
   useEffect(() => {
-    console.log("effect");
+    console.log("country effect");
     axios
       .get("https://restcountries.com/v3.1/all")
       .then(response => {
-        console.log("promise fulfilled");
+        console.log("country promise fulfilled");
         setCountries(response.data) 
       })
   }, [])
@@ -36,7 +37,8 @@ const App = () => {
     <div>
       <Filter handleFilterChange={handleFilterChange}/>
       <Display countriesToShow={countriesToShow} chosenCountry={chosenCountry} 
-      handleChooseCountry={handleChooseCountry}/>
+      handleChooseCountry={handleChooseCountry} weatherInfo={weatherInfo} 
+      setWeatherInfo={setWeatherInfo}/>
     </div>
   )
 }
@@ -50,7 +52,7 @@ const Filter = ({handleFilterChange}) => {
   )
 }
 
-const Display = ({countriesToShow, chosenCountry, handleChooseCountry}) => {
+const Display = ({countriesToShow, chosenCountry, handleChooseCountry, weatherInfo, setWeatherInfo}) => {
   const numCountries = countriesToShow.length
   if (numCountries > 10) {
     return (
@@ -59,7 +61,7 @@ const Display = ({countriesToShow, chosenCountry, handleChooseCountry}) => {
       </div>
     )
   }
-  if (numCountries <= 10 && numCountries > 1) {
+  if (numCountries <= 10 && numCountries > 1 && Object.keys(chosenCountry).length !== 0) {
     return (
       <div>
         <ul>
@@ -69,7 +71,21 @@ const Display = ({countriesToShow, chosenCountry, handleChooseCountry}) => {
             </li>
           )}
         </ul>
-        <CountryInfo country={chosenCountry} />
+        <CountryInfo country={chosenCountry} weatherInfo={weatherInfo} 
+        setWeatherInfo={setWeatherInfo}/>
+      </div>
+    )
+  }
+  if (numCountries <= 10 && numCountries > 1 && Object.keys(chosenCountry).length === 0) {
+    return (
+      <div>
+        <ul>
+          {countriesToShow.map(country => 
+            <li key={country.name.official}>
+              {country.name.common} <Button clickHandler={handleChooseCountry} country={country} />
+            </li>
+          )}
+        </ul>
       </div>
     )
   }
@@ -83,19 +99,31 @@ const Display = ({countriesToShow, chosenCountry, handleChooseCountry}) => {
 
   return (
     <div>
-      <CountryInfo country={countriesToShow[0]} />
+      <CountryInfo country={countriesToShow[0]} weatherInfo={weatherInfo} 
+      setWeatherInfo={setWeatherInfo}/>
     </div>
   )
 }
 
-const CountryInfo = ({country}) => {
-  if (Object.keys(country).length === 0) {
-    return (
-      <div>
+const CountryInfo = ({country, weatherInfo, setWeatherInfo}) => {
 
-      </div>
-    )
-  }
+  const apiKey = process.env.REACT_APP_API_KEY
+  const lat = country.capitalInfo.latlng[0]
+  const lon = country.capitalInfo.latlng[1]
+
+  console.log(country);
+
+  useEffect(() => {
+    console.log("weather effect");
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+      .then(response => {
+        console.log("weather promise fulfilled");
+        console.log(response.data);
+        setWeatherInfo(response.data)
+      })
+  }, [])
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -109,10 +137,33 @@ const CountryInfo = ({country}) => {
         {Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>)}
       </ul>
       <img src={country.flags.png} />
+      <Weather country={country} weatherInfo={weatherInfo} setWeatherInfo={setWeatherInfo} />
     </div>
   )
 }
 
 const Button = ({clickHandler, country}) => <button onClick={() => clickHandler(country)}>show</button>
+
+const Weather = ({country, weatherInfo}) => {
+
+  if (Object.keys(country).length === 0 || Object.keys(weatherInfo).length === 0) {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+  const temp = weatherInfo.main.temp - 273.15
+
+  return (
+    <div>
+      <h2>Weather in {country.capital[0]}</h2>
+      <p>Temperature: {temp} °C</p>
+      <img src={`http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`} />
+      <p>Wind: </p>
+    </div>
+  )
+}
 
 export default App;
